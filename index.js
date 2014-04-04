5dca302967350598518cdc316c2e680707a72b76
Amber Dixon <amberdixon@squareup.com>
2014-04-04 14:45:43 -0700

    Add JSDocs.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var fs = require('fs');
 
+/**
+ * @param {string} caCertFilename The filename of a ca cert file.
+ * @return {array} A list of PEM strings, one for each PEM in the passed in file.
+ */
 function loadCACertsFromFile(caCertFilename) {
   var caCertFileContents = fs.readFileSync(caCertFilename).toString();
   return PEMParser.loadCACerts(caCertFileContents);
 }
 
+/**
+ * @param {string} caCertFileContents The contents of a cert file.
+ * @return {array} A list of PEM strings, one for each PEM in the passed in file.
+ */
 function loadCACerts(caCertFileContents) {
   var END_CERT = '-----END CERTIFICATE-----';
       sections = caCertFileContents.split(END_CERT).slice(0, -1);
